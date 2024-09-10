0d0711557e1173c2cc472d71b0b8bb0bbdb30d9d
erikbrignall <31072339+erikbrignall@users.noreply.github.com>
2024-09-10 09:38:45 +0100

    basic file uploads to get started

---
+++ vector_methods_scoring.py
@@ -0,0 +1,23 @@
+# python -m streamlit run vector_methods_scoring.py
+
+import pandas as pd
+import streamlit as st
+# LANGUAGE DETECTION
+from lingua import Language, LanguageDetectorBuilder
+# reading ease score
+import textstat
+
+st.set_page_config(page_title='Text Assessment Estimation - DEMO')
+
+st.image('TR-logo.png')
+st.title('Methods Assessment - VECTOR DEMO')
+st.write('The following is a demo for text quality scoring.')
+vehicle_name = st.text_input('Methods text')
+
+st.subheader('Language Detected:')
+
+st.subheader('Word Count:')
+
+st.subheader('Readability: (Flesch Reading Ease Score)')
+
+st.subheader('Grammar errors:')

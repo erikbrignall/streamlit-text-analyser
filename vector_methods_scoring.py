aac9effd5679b7a0704141fd40deef9ce0f233b0
erikbrignall <31072339+erikbrignall@users.noreply.github.com>
2024-09-10 09:55:08 +0100

    Update vector_methods_scoring.py

--- vector_methods_scoring.py
+++ vector_methods_scoring.py
@@ -7,7 +7,6 @@ import textstat
 
 st.set_page_config(page_title='Text Assessment Estimation - DEMO')
 
-st.image('TR-logo.png')
 st.title('Methods Assessment - VECTOR DEMO')
 st.write('The following is a demo for text quality scoring.')
 vehicle_name = st.text_input('Methods text')
